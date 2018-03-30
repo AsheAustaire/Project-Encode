af653e23c44ea90611ff6dc259a637b1d65a9867
Ashe Austaire <AsheAustaire@gmail.com>
2018-03-30 16:10:58 -0400

    refactored_algorithm

--- components/pane/pane_user-input/pane_user-input.js
+++ components/pane/pane_user-input/pane_user-input.js
@@ -1,38 +1,122 @@
-const userInputPane = {
-  pane: document.body.querySelector('#pane_user-input'),
-  nextPane: document.body.querySelector('#pane_encoding-selection'),
-  form: document.body.querySelector('#usr-input-form'),
-  input: document.body.querySelector('#usr-input-input'),
-  encodingText: document.body.querySelector('#usr-input-encoding-text'),
-  binaryText : document.body.querySelector('#usr-input-binary-text')
-}
-
-userInputPane.form.addEventListener('submit', (ev) => {
-  ev.preventDefault();
-  userInputPane.pane.classList.add("fadeOut");
-  userInputPane.nextPane.classList.add("visible");
-  userInputPane.nextPane.classList.add("fadeIn");
-});
-
-userInputPane.input.addEventListener('input', (ev) => {
-  if(userInputPane.input.value === '' || userInputPane.input.value === null) {
-    userInputPane.encodingText.innerText = 'none';
-    userInputPane.binaryText.innerText = 'none';
-  } else {
-    userInputPane.encodingText.innerText = returnEncodingTypes(userInputPane.input.value.charCodeAt().toString(2))
-    userInputPane.binaryText.innerText = userInputPane.input.value.charCodeAt().toString(2);
+(function(){
+  const userInputPane = {
+    pane: document.body.querySelector('#pane_user-input'),
+    nextPane: document.body.querySelector('#pane_encoding-selection'),
+    form: document.body.querySelector('#usr-input-form'),
+    input: document.body.querySelector('#usr-input-input'),
+    encodingText: document.body.querySelector('#usr-input-encoding-text'),
+    binaryText : document.body.querySelector('#usr-input-binary-text')
   }
-  console.log(ev);
-})
 
-function returnEncodingTypes(binary) {
-  return checkEncdoingType(binary)
-}
+  userInputPane.form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    userInputPane.pane.classList.add("fadeOut");
+    userInputPane.nextPane.classList.add("visible");
+    userInputPane.nextPane.classList.add("fadeIn");
+  });
 
-function checkEncdoingType(binary) {
-  if(binary.length === 7) {
-    return 'UTF-8 or ASCII'
-  } else {
-    return 'Unknown Encoding'
+  userInputPane.input.addEventListener('input', (ev) => {
+    if(userInputPane.input.value === '' || userInputPane.input.value === null) {
+      userInputPane.encodingText.innerText = 'none';
+      userInputPane.binaryText.innerText = 'none';
+    } else {
+      displayEncoding()
+    }
+    console.log(ev);
+  })
+
+// Places Text Onto Screen
+
+  function displayEncoding() {
+    let encodingObj = getEncodingInfo(userInputPane.input.value.charCodeAt().toString(2));
+    userInputPane.encodingText.innerText =  encodingObj.encodingText;
+    userInputPane.binaryText.innerText =  encodingObj.binaryText;
   }
-}
+
+// Sets Encoding Obj w/ Appropriate Data
+
+  function getEncodingInfo(binaryText) {
+    let encodingObj = createEncodingObj();
+    encodingObj.encodingText = returnEncodingText(binaryText)
+    encodingObj.binaryText = returnBinaryText(binaryText)
+    return encodingObj
+  }
+
+// Creates Empty Encoding Obj
+
+  function createEncodingObj() {
+    return {
+      binaryText: '',
+      encodingText: ''
+    };
+  }
+
+// Returns String For Encoding Text
+
+  function returnEncodingText(binaryText) {
+    let encodedTextString = [];
+    if(isUTF8(binaryText)) {
+      encodedTextString.push('UTF-8')
+    }
+    if(isUTF16(binaryText)) {
+      encodedTextString.push('UTF-16')
+    }
+    if(isUTF32(binaryText)) {
+      encodedTextString.push('UTF-32')
+    }
+    if(isASCII(binaryText)) {
+      encodedTextString.push('ASCII')
+    }
+    if(encodedTextString === '') {
+      encodedTextString.push('Could Not Identify')
+    }
+
+    return encodedTextString.join(' or ');
+  }
+
+// Checks Binary Text Against Encoding Type Traits
+    function isUTF8(binaryText) {
+      if(binaryText.length < 8){
+        return true
+      }
+      return true;
+    }
+// Checks Binary Text Against Encoding Type Traits
+    function isUTF16(binaryText) {
+      if(binaryText.length > 8 && binaryText <= 16){
+        return true;
+      }
+    }
+// Checks Binary Text Against Encoding Type Traits
+    function isUTF32(binaryText) {
+      if(binaryText.length > 16 && binaryText <= 32){
+        return true;
+      }
+    }
+// Checks Binary Text Against Encoding Type Traits
+    function isASCII(binaryText) {
+      if(binaryText.length < 8){
+        return true;
+      }
+    }
+// Checks Binary Text Against Encoding Type Traits
+    function isBIG5(binaryText) {
+      if(binaryText.length == 16){
+        return true;
+      }
+    }
+
+// Returns String For Binary Text
+
+  function returnBinaryText(binaryText) {
+    newBinaryText = binaryText
+    if(newBinaryText.length < 8) {
+      let missingZero = 8 - binaryText.length
+      for(let i = 0; i < missingZero; i++)
+      newBinaryText = '0' + newBinaryText
+    }
+    return newBinaryText
+  }
+
+
+}())
